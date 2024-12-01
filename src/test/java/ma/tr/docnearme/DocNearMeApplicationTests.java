ee637e24eb33f2586cd9f5dd1386005dd48020df
mohamed tergui <simotergui4@gmail.com>
2024-12-01 22:12:04 +0100

    init the project structur

---
+++ src/test/java/ma/tr/docnearme/DocNearMeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.tr.docnearme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocNearMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
